4ca9bcc79464293ea87d401d99cf3b65d6280286
Tom-chack <tom.controlcenter@gmail.com>
2022-02-04 18:54:48 +0400

    eslint and prettier

--- src/App.js
+++ src/App.js
@@ -11,17 +11,16 @@ import Register from "./components/Register";
 
 
 function App() {
-  
-  return (
 
-        <Routes>
-          <Route path="/" element={<Structure/>}>
-            <Route index element={<Home />}/>
-            <Route path="/members" element={<Members />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/register" element={<Register />}/>
-          </Route>
-        </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Structure />}>
+        <Route index element={<Home />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Structure.js
+++ src/components/Structure.js
@@ -6,29 +6,29 @@ const { Header, Content, Footer } = Layout;
 function Structure() {
   return (
     <Layout className="layout">
-        <Header>
+      <Header>
         <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item  key="1">
-                  <Link to="/"><span>Home</span></Link>
-                </Menu.Item>
-                <Menu.Item  key="2">
-                  <Link to="/members"><span>Members</span></Link>
-                </Menu.Item>
-                <Menu.Item  key="3">
-                  <Link to="/login"><span>Login</span></Link>
-                </Menu.Item>
-                <Menu.Item  key="4">
-                  <Link to="/register"><span>Register</span></Link>
-                </Menu.Item>
-            </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-            <div className="site-layout-content">
-              <Outlet />
-            </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Social Network by Group 3</Footer>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">
+            <Link to="/"><span>Home</span></Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/members"><span>Members</span></Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/login"><span>Login</span></Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Link to="/register"><span>Register</span></Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <Outlet />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Social Network by Group 3</Footer>
     </Layout>
   );
 }
