ae357aee1fae77eba8c14bdc3d9b6c554890422a
Mariam <mariammdanielyan@gmail.com>
2022-02-12 00:18:44 +0400

    User card

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from 'react-router-dom'
+import { dateToLocalFormat } from 'date-format-ms';
 import { useEffect } from "react";
 import getUsers from '../services/getUsers'
 import './members.css'
@@ -13,12 +15,17 @@ function Members() {
       dispatch(getUsers());
   }, [dispatch])
 
+
+  const getDate = (sec) =>{
+   return dateToLocalFormat(new Date(sec), 'd.m.Y')
+  }
+
   return (
     <div className="members-container">
       
       <div className="members-content">
         
-        <div className="search-div">
+        <div className="search-container">
           
           <span>Members</span>
           
@@ -26,6 +33,41 @@ function Members() {
             type={'text'} 
             placeholder={'Search . . .'}
           />
+
+        </div>
+
+        <div className="users-container">
+
+          <div className="background-part">
+            <img 
+              src="https://images.unsplash.com/photo-1547989453-11e67ffb3885?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
+              alt="bg-img"
+            />
+            
+            <div className="img-container">
+
+              <img
+               alt="avatar"
+               src="https://qph.fs.quoracdn.net/main-thumb-1278318002-200-ydzfegagslcexelzgsnplcklfkienzfr.jpeg"
+              />   
+
+            </div>
+
+          </div>
+
+          <div className="info-part">
+
+            <div>
+
+              <Link to={'/'}>
+                <span>Clara Tomson</span>
+              </Link>
+
+              <span style={{color: "grey"}}>{`Joined ${getDate(1519211809934)}`}</span>
+
+            </div>
+
+          </div>
 
         </div>
 
