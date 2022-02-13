d2a87708e3018ddeaebcc98a98d67e9c5fc3a868
Tom-chack <tom.controlcenter@gmail.com>
2022-02-13 20:16:43 +0400

    Fetch user, image and comemnts of each post  when we use getPosts()

--- src/components/profile/About.js
+++ src/components/profile/About.js
@@ -1,33 +1,9 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import getPosts from "../../services/getPosts";
+import React from "react";
 
 function About() {
-  const dispatch = useDispatch();
-  const { profile } = useSelector((state) => state.userDuck);
-  const { posts } = useSelector((state) => state.postDuck);
-
-  //Fetch user object by profile user id
-  useEffect(() => {
-    if (profile.id) {
-      dispatch(getPosts("?userid=" + profile.id, true));
-    } else {
-      console.log("no posts found");
-    }
-  }, [dispatch, profile]);
-
   return (
     <div>
-      <p>Username: {profile.username}</p>
-      <p>Twitter: {profile.tw}</p>
-      <p>ID: {profile.id}</p>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <p>Content: {post.content}</p>
-          <p>Image: {post?.image}</p>
-          <hr />
-        </div>
-      ))}
+      <span>About Component</span>
     </div>
   );
 }
--- src/services/getPosts.js
+++ src/services/getPosts.js
@@ -2,29 +2,62 @@ import api from "../helpers/api";
 import { loadPosts, postError } from "../redux/ducks/postDuck";
 
 const getPosts =
-  (query = "", images = false, comments = false) =>
+  (query = "", user = true, images = true, comments = true) =>
   async (dispatch) => {
     try {
+      // Get posts filtered by query
       const postRes = await fetch(`${api}/posts${query}`);
       const postData = await postRes.json();
-      if (images) {
-        for (let p = 0; p < postData.length; p++) {
-          postData[p].image = await getImage(postData[p], dispatch);
-        }
+
+      // Get post user, image and comments
+      for (let i = 0; i < postData.length; i++) {
+        if (user) postData[i].user = await getUser(postData[i], dispatch);
+        if (images) postData[i].image = await getImage(postData[i], dispatch);
+        if (comments) postData[i].comments = await getComments(postData[i], dispatch);
       }
-      console.log(postData);
+
+      // Add posts to store state ....................................
       dispatch(loadPosts(postData));
     } catch (e) {
       dispatch(postError(e.message));
     }
   };
 
+//Function to fetch post user by post userid
+async function getUser(post, dispatch) {
+  if (post) {
+    try {
+      let userRes = await fetch(`${api}/users?userid=${post.userid}`);
+      let userData = await userRes.json();
+      return userData[0];
+    } catch (e) {
+      dispatch(postError(e.message));
+    }
+  }
+  return "";
+}
+
+//Function to fetch images by postid
 async function getImage(post, dispatch) {
   if (post) {
     try {
       let imageRes = await fetch(`${api}/images?postid=${post.id}`);
       let imageData = await imageRes.json();
-      return imageData?.url;
+      return imageData[0]?.url;
+    } catch (e) {
+      dispatch(postError(e.message));
+    }
+  }
+  return "";
+}
+
+//Function to fetch comments by postid
+async function getComments(post, dispatch) {
+  if (post) {
+    try {
+      let commentRes = await fetch(`${api}/comments?postid=${post.id}`);
+      let commentData = await commentRes.json();
+      return commentData;
     } catch (e) {
       dispatch(postError(e.message));
     }
