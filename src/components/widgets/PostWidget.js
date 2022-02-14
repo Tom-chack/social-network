62d3c53eb834a354efc18f8e9d6d4674bd721caa
Mariam <mariammdanielyan@gmail.com>
2022-02-14 20:47:51 +0400

    Widgets

--- src/components/widgets/PostWidget.js
+++ src/components/widgets/PostWidget.js
@@ -1,7 +1,27 @@
-
+import getPosts from '../../services/getPosts'
+import getUsers from '../../services/getUsers';
+import { useState,useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from 'react-router-dom';
 
 
 const PostWidget = () =>{
+  
+const dispatch = useDispatch();
+const { posts } = useSelector((state) => state.postDuck);
+const { users } = useSelector((state) => state.userDuck);
+
+
+useEffect(() => {
+    
+    dispatch(getPosts("?_sort=likes&_order=desc&_star=1&_end=5"));
+    
+  }, [dispatch]);
+  
+
+
+
+
     return(
         <div className="post-widget-content">
 
@@ -14,6 +34,32 @@ const PostWidget = () =>{
                 </div>
 
             </div>
+
+            {
+                posts.map(post=>(
+
+                    <div key={post.id} className='post-element'>
+
+                        <div className='post-avatar'>
+
+                            <img src={''} alt={''}/>
+
+                        </div>
+
+                        <div className='post-content'>
+
+                            <Link to={`/posts/${post.id}`}>
+
+                            <span>{`${post.content.slice(0,60)} . . .`}</span>
+
+                            </Link>
+
+                        </div>
+
+                        
+                    </div>
+                ))
+            }
 
         </div>
     )
