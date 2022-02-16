27e37cecdc20cb681c18dff56e88cd003d2abaff
Tom-chack <tom.controlcenter@gmail.com>
2022-02-16 12:36:03 +0400

    New actions: add, update and delete post

--- src/redux/ducks/postDuck.js
+++ src/redux/ducks/postDuck.js
@@ -2,10 +2,16 @@ import { postSchema } from "../../helpers/schemas";
 
 // Action Types
 const LOAD_POSTS = "post/LOAD_POSTS";
+const ADD_POST = "post/ADD_POST";
+const UPDATE_POST = "post/UPDATE_POST";
+const DELETE_POST = "post/DELETE_POST";
 const POST_ERROR = "post/POST_ERROR";
 
 // Actions
 export const loadPosts = (payload) => ({ type: LOAD_POSTS, payload });
+export const addPost = (payload) => ({ type: ADD_POST, payload });
+export const updatePost = (payload) => ({ type: UPDATE_POST, payload });
+export const deletePost = (payload) => ({ type: DELETE_POST, payload });
 export const postError = (payload) => ({ type: POST_ERROR, payload });
 
 // Initial State of the postDuck
@@ -22,6 +28,24 @@ const postDuck = (state = initialState, { type, payload }) => {
       return {
         ...state,
         posts: payload,
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, { ...postSchema, payload }],
+      };
+    case UPDATE_POST:
+      return {
+        ...state,
+        posts: state.posts.map((post) => {
+          if (post.id === payload.id) post = { ...post, payload };
+          return post;
+        }),
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => !post.id === payload.id),
       };
     case POST_ERROR:
       return {
