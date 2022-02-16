4db2bbec079e629e9971f7362f726146400aeee0
Tom-chack <tom.controlcenter@gmail.com>
2022-02-16 13:28:48 +0400

    New services addPost(), updatePost(), deletePost()

--- src/redux/ducks/postDuck.js
+++ src/redux/ducks/postDuck.js
@@ -2,16 +2,16 @@ import { postSchema } from "../../helpers/schemas";
 
 // Action Types
 const LOAD_POSTS = "post/LOAD_POSTS";
-const ADD_POST = "post/ADD_POST";
-const UPDATE_POST = "post/UPDATE_POST";
-const DELETE_POST = "post/DELETE_POST";
+const POST_ADD = "post/POST_ADD";
+const POST_UPDATE = "post/POST_UPDATE";
+const POST_DELETE = "post/POST_DELETE";
 const POST_ERROR = "post/POST_ERROR";
 
 // Actions
 export const loadPosts = (payload) => ({ type: LOAD_POSTS, payload });
-export const addPost = (payload) => ({ type: ADD_POST, payload });
-export const updatePost = (payload) => ({ type: UPDATE_POST, payload });
-export const deletePost = (payload) => ({ type: DELETE_POST, payload });
+export const postAdd = (payload) => ({ type: POST_ADD, payload });
+export const postUpdate = (payload) => ({ type: POST_UPDATE, payload });
+export const postDelete = (payload) => ({ type: POST_DELETE, payload });
 export const postError = (payload) => ({ type: POST_ERROR, payload });
 
 // Initial State of the postDuck
@@ -29,12 +29,12 @@ const postDuck = (state = initialState, { type, payload }) => {
         ...state,
         posts: payload,
       };
-    case ADD_POST:
+    case POST_ADD:
       return {
         ...state,
         posts: [...state.posts, { ...postSchema, payload }],
       };
-    case UPDATE_POST:
+    case POST_UPDATE:
       return {
         ...state,
         posts: state.posts.map((post) => {
@@ -42,10 +42,10 @@ const postDuck = (state = initialState, { type, payload }) => {
           return post;
         }),
       };
-    case DELETE_POST:
+    case POST_DELETE:
       return {
         ...state,
-        posts: state.posts.filter((post) => !post.id === payload.id),
+        posts: state.posts.filter((post) => !post.id === payload),
       };
     case POST_ERROR:
       return {
