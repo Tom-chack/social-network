c9dc740a13d449ac6ff6c3dc6dfa441ad4e27737
Tom-chack <tom.controlcenter@gmail.com>
2022-02-27 14:50:36 +0400

    Add db.json for DEMO

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Menu } from "antd";
 import { menus } from "../helpers/menus";
 import { userLogout } from "../redux/ducks/userDuck";
+import api from "../helpers/api";
 
 function Menus() {
   const location = useLocation();
@@ -11,6 +12,19 @@ function Menus() {
 
   const dispatch = useDispatch();
   const { loggedIn, user } = useSelector((state) => state.userDuck);
+
+  const [loginUser, setLoginUser] = useState({});
+
+  useEffect(() => {
+    fetch(`${api}/users/${user.id}`)
+      .then((user) => user.json())
+      .then((user) => {
+        setLoginUser(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, [setLoginUser, user]);
 
   useEffect(() => {
     setCurrentPath(location.pathname);
@@ -46,6 +60,17 @@ function Menus() {
             )}
           </Menu.Item>
         ))}
+      {loggedIn && (
+        <Menu.Item key='100' className='menu-profile' style={{ backgroundColor: "transparent" }}>
+          <Link to={"/profile/" + user.id}>
+            <img
+              src={loginUser.avatar}
+              alt={loginUser.name || loginUser.username}
+              style={{ width: "40px", borderRadius: "50%", border: "2px #f5f5f5 solid" }}
+            />
+          </Link>
+        </Menu.Item>
+      )}
     </Menu>
   );
 }
